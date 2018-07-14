991f671b81dfc236c61167873657a4c94fe3aa82
Sarawut Positwinyu <sarawut.positwinyu@gmail.com>
2018-07-14 10:19:50 +0700

    Add Asp.NET MVC Web Application

---
+++ MovieRecordsManagement.WebMVC/Controllers/MovieController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieRecordsManagement.DAL.Domains;
+using MovieRecordsManagement.DAL.Repositories;
+using MovieRecordsManagement.WebMVC.Models;
+
+namespace MovieRecordsManagement.WebMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        private IRepository<MovieRecord> _movieRecordRepository;
+
+        public MovieController(IRepository<MovieRecord> movieRecordRepository)
+        {
+            this._movieRecordRepository = movieRecordRepository;
+        }
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movieViewModels = this._movieRecordRepository.GetAll().Select(item => new MovieViewModel(item));
+            return View(movieViewModels);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MovieRecordsManagement.WebMVC/Models/MovieViewModel.cs
@@ -0,0 +1,26 @@
+﻿using MovieRecordsManagement.DAL.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRecordsManagement.WebMVC.Models
+{
+    public class MovieViewModel
+    {
+        private MovieRecord _movieRecord;
+
+        public string Id { get; set; }
+        public string MovieTitle { get; set; }
+        public int YearReleased { get; set; }
+        public string Rating { get; set; }
+
+        public MovieViewModel(MovieRecord movieRecord)
+        {
+            this._movieRecord = movieRecord;
+            this.MovieTitle = movieRecord.MovieTitle;
+            this.YearReleased = movieRecord.YearReleased;
+            this.Rating = movieRecord.Rating;
+        }
+    }
+}
