5d880ed2e10bc3488a6278af04f445d0e8191c5d
Sarawut Positwinyu <sarawut.positwinyu@gmail.com>
2018-07-15 22:33:10 +0700

    remove unnecessary code

--- MovieRecordsManagement.DAL/Domains/User.cs
+++ MovieRecordsManagement.DAL/Domains/User.cs
@@ -7,10 +7,6 @@ namespace MovieRecordsManagement.DAL.Domains
 {
     public class User
     {
-        [Key]
-        public string UserName;
-        public string Role;
-
         public static class ROLE_CONST
         {
             public const string MANAGER = "MANAGER";
